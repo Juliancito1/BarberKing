62c638de41ef0b7b8700c49822c1727a2df54024
Julian <julianser@hotmail.com.ar>
2024-10-23 01:36:33 -0300

    Se realizo el Banner de la pagina principal

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import Banner1 from "../img/Banner1.jpg";
+import Banner2 from "../img/Banner2.jpg";
+import Banner3 from "../img/Banner3.jpg";
 const Banner = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Carousel className="carousel" fade>
+      <Carousel.Item>
+        <img
+          className="carouselImg img-fluid"
+          src={Banner1}
+          text="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="carouselImg img-fluid"
+          src={Banner2}
+          text="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="carouselImg img-fluid"
+          src={Banner3}
+          text="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
     <section className="mainSection">
-      <h1>Hola</h1>
+      <Banner/>
     </section>
   );
 };
