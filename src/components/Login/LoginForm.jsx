fe83cce8c3905efc2adddc4f67f6b9fb188b2ccd
Julian <julianser@hotmail.com.ar>
2024-10-26 19:36:42 -0300

    Se realizo la logica del formulario y se agregaron los links para olvide mi contraseña y registrarse

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,31 +1,88 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const login = (formData) => {
+    console.log(formData)
+  }
+
   return (
     <section>
-      <Form className="fondoArticulo text-light p-3 rounded-5">
+      <Form
+        className="fondoArticulo text-light p-3 rounded-5"
+        onSubmit={handleSubmit(login)}
+      >
         <Form.Group className="mb-3">
           <Form.Label className="fs-5">Nombre y Apellido</Form.Label>
           <Form.Control
             className="formInput"
-            required
             type="text"
             placeholder="Ej. Juan Perez"
+            {...register("nombreapellido", {
+              required: "El nombre y apellido es obligatorio",
+              minLength: {
+                value: 3,
+                message: "Minimo 3 carácteres",
+              },
+              maxLength: {
+                value: 30,
+                message: "Maximo 30 carácteres",
+              },
+              pattern: {
+                value: /^[A-Za-z\s]{3,30}$/,
+              },
+            })}
           ></Form.Control>
+          <div className="text-danger">{errors.nombreapellido?.message}</div>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label className="fs-5">Contraseña</Form.Label>
           <Form.Control
             className="formInput"
-            required
             type="text"
             placeholder="Ej. juan123"
+            {...register("password", {
+              required: "La contraseña es obligatoria",
+              minLength: {
+                value: 8,
+                message: "La contraseña debe tener un mínimo de 8 carácteres",
+              },
+              maxLength: {
+                value: 30,
+                message: "La contraseña debe tener un máximo 30 carácteres",
+              },
+              pattern: {
+                value: /^[A-Za-z0-9!@#$%^&*()_\-+=\[\]{}|:;,.?/~]{8,}$/,
+                message:
+                  "La contraseña puede contener letras, numeros y carácteres especiales",
+              },
+            })}
           ></Form.Control>
+          <div className="text-danger">{errors.password?.message}</div>
         </Form.Group>
+        <div className="text-end">
+          <Button type="submit">Ingresar</Button>
+        </div>
+        <h6>
+          ¿Olvidaste tu contraseña?{" "}
+          <Link className="text-decoration-none">Haz Click Aqui</Link>
+        </h6>
+        <h6>
+          ¿No tienes una cuenta?{" "}
+          <Link className="text-decoration-none">Registrate Aqui</Link>
+        </h6>
       </Form>
     </section>
   );
-}
+};
 
-export default LoginForm
+export default LoginForm;
